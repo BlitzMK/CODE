47045c2c8c9ca46fbb51d98dc893721fdce1e2b2
Samuel Chandler <sec180005@utdallas.edu>
2022-04-05 01:42:37 -0500

    just a base commit

---
+++ Projects/NewsPaper/Main.py
@@ -0,0 +1,19 @@
+# Goal: Ceate a program that will gather information about topics the user is interested in from multiple news sources through the News_api 
+# Samuel Chandler 
+# News API key: 92eddc39963c4b7b926019ab01e0fe0e
+# News API link: https://newsapi.org/
+
+
+from newsapi import NewsApiClient
+api = NewsApiClient(api_key='92eddc39963c4b7b926019ab01e0fe0e')
+
+# Get a list of news sources
+def GetNews():
+    x=0
+
+
+
+
+#Base
+GetNews()
+
