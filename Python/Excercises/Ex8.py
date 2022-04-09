dcbd60752d1a93d032e3af956fe75db21b66b658
Samuel Chandler <sec180005@utdallas.edu>
2022-04-09 16:01:39 -0500

    6 -8

---
+++ Python/Excercises/Ex8.py
@@ -0,0 +1,6 @@
+# Make a two-player Rock-Paper-Scissors game. 
+print("Welcome to RPS choose 'R' Rock, 'P' Paper, and 'S' Scizzors")
+
+def input():
+    p1 = input("Player 1: ")
+    p2 = input("Player 2: ")
